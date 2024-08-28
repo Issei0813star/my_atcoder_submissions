4a7a15bb3bc7a1ca2e7eed946793e7f91f66c11d
Issei0813star <issei0813star.dev@gmail.com>
2024-08-29 04:23:22 +0900

    my AtCoder submissions

--- codes/abc367/abc367_a.java
+++ codes/abc367/abc367_a.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int A = sc.nextInt();  // 叫ぶ時間
-        int B = sc.nextInt();  // 寝る時間
-        int C = sc.nextInt();  // 起きる時間
-        
-        if (B < C) {  // 寝る時間と起きる時間が同じ日に収まる場合
-            if (B <= A && A < C) {
-                System.out.println("No");
-            } else {
-                System.out.println("Yes");
-            }
-        } else {  // 日を跨ぐ場合
-            if (A >= B || A < C) {
-                System.out.println("No");
-            } else {
-                System.out.println("Yes");
-            }
-        }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int sakebu = sc.nextInt();
+    int neru = sc.nextInt();
+    int okiru = sc.nextInt();
+    
+    boolean matagu = neru > okiru;
+    
+    if(!matagu) {
+      if(neru < sakebu && sakebu < okiru) {
+        System.out.println("No");
+      }
+      else {
+        System.out.println("Yes");
+      }
     }
+    else {
+      if(sakebu < okiru || sakebu > neru) {
+        System.out.println("No");
+      }
+      else {
+        System.out.println("Yes");
+      }
+    }
+    
+  }
 }
