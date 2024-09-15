e49ccc1b1d41531a3f9aebf8c1736a4bd7e976a4
Issei0813star <issei0813star.dev@gmail.com>
2024-09-15 13:40:48 +0900

    my AtCoder submissions

--- codes/abc371/abc371_c.cpp
+++ codes/abc371/abc371_c.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 // グラフを隣接行列で表現
 vector<vector<int>> adjG, adjH, A;
+
+// 与えられたパーミュテーションに基づき、HのグラフとGのグラフを同型にするためのコストを計算
 int calculate_cost(vector<int>& perm, int N) {
     int cost = 0;
     for (int i = 0; i < N; i++) {
@@ -22,32 +24,37 @@ int main() {
     int N, MG, MH;
     cin >> N >> MG;
 
+    // グラフGとHの隣接行列を初期化
     adjG.assign(N, vector<int>(N, 0));
     adjH.assign(N, vector<int>(N, 0));
     A.assign(N, vector<int>(N, 0));
 
+    // Gの辺の情報を読み込み
     for (int i = 0; i < MG; i++) {
         int u, v;
         cin >> u >> v;
-        u--; v--;
+        u--; v--;  // 0-indexed に変換
         adjG[u][v] = adjG[v][u] = 1;
     }
 
+    // Hの辺の情報を読み込み
     cin >> MH;
     for (int i = 0; i < MH; i++) {
         int a, b;
         cin >> a >> b;
-        a--; b--; 
+        a--; b--;  // 0-indexed に変換
         adjH[a][b] = adjH[b][a] = 1;
     }
 
+    // コスト行列の読み込み
     for (int i = 0; i < N; i++) {
         for (int j = i + 1; j < N; j++) {
             cin >> A[i][j];
-            A[j][i] = A[i][j];
+            A[j][i] = A[i][j];  // 対称行列
         }
     }
 
+    // 頂点の順列（パーミュテーション）を作成
     vector<int> perm(N);
     for (int i = 0; i < N; i++) {
         perm[i] = i;
@@ -55,11 +62,13 @@ int main() {
 
     int min_cost = INT_MAX;
 
+    // 頂点の全ての並べ替えを試して、最小コストを求める
     do {
         int current_cost = calculate_cost(perm, N);
         min_cost = min(min_cost, current_cost);
     } while (next_permutation(perm.begin(), perm.end()));
 
+    // 最小コストを出力
     cout << min_cost << endl;
 
     return 0;
